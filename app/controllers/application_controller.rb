8d96490df2250694a5af92e05a6693ebd9e0768b
anastasia-sn7 <melaniefed03@gmail.com>
2023-05-13 18:36:08 +0300

    if you're logged out you can only log in or sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    redirect_to root_path unless request.path == new_patient_session_path || request.path == new_doctor_session_path ||
+      request.path == root_path || request.path == new_patient_registration_path ||
+      request.path == edit_patient_registration_path || request.path == new_admin_user_session_path ||
+      patient_signed_in? || doctor_signed_in?
+  end
 end
