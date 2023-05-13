7fe8fc02c8e836f998615aa21a331dd72bbeb922
anastasia-sn7 <melaniefed03@gmail.com>
2023-05-13 16:25:39 +0300

    cancel appointments when dates are unavailable

---
+++ app/services/cancel_appointments_service.rb
@@ -0,0 +1,11 @@
+class CancelAppointmentsService
+  def self.call(doctor, start_date, end_date)
+    dates = (start_date..end_date).step(1).to_a
+    dates.each do |date|
+      appointments = doctor.appointments.where(appointment_date: date)
+      appointments.each do |appointment|
+        appointment.update(status: -1)
+      end
+    end
+  end
+end
